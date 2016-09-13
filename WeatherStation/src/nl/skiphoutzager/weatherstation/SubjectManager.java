b91728f1e33aa8a8e83ddec183052fb7785065c0
Skip <skip.houtzager@gmail.com>
2016-09-13 14:36:27 +0200

    Changed ArrayList type to Collection

--- WeatherStation/src/nl/skiphoutzager/weatherstation/SubjectManager.java
+++ WeatherStation/src/nl/skiphoutzager/weatherstation/SubjectManager.java
@@ -1,10 +1,11 @@
 package nl.skiphoutzager.weatherstation;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class SubjectManager<T> {
 
-	private ArrayList<Observer<T>> observerList = new ArrayList<Observer<T>>();
+	private Collection<Observer<T>> observerList = new ArrayList<Observer<T>>();
 
 	public void registerObserver(Observer<T> observer) {
 		observerList.add(observer);
